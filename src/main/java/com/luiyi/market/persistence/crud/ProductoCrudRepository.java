a2adfa019e8a91561ad188ed470440767613eca2
Luiyi <luiyiantonyqp@gmail.com>
2024-03-04 22:59:22 -0500

    Query methods

--- src/main/java/com/luiyi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/luiyi/market/persistence/crud/ProductoCrudRepository.java
@@ -1,7 +1,17 @@
 package com.luiyi.market.persistence.crud;
 
 import com.luiyi.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(Integer idCategoria);
+
+    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> getByCategoria(Integer idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(Integer cantidadStock, Boolean estado);
 }
