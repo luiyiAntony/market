97011f16ff1605182203d215c793d8e9caa8d26a
Luiyi <luiyiantonyqp@gmail.com>
2024-03-05 21:47:42 -0500

    Agregando @Repository y terminado el ProductRepository (getProduct, save, delete)

--- src/main/java/com/luiyi/market/persistence/ProductoRepository.java
+++ src/main/java/com/luiyi/market/persistence/ProductoRepository.java
@@ -2,10 +2,14 @@ package com.luiyi.market.persistence;
 
 import com.luiyi.market.persistence.crud.ProductoCrudRepository;
 import com.luiyi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository // le indicamos a Spring que esta clase se encarga de interactuar con la base de datos
+//@Component // Es una generalizacion (Repository es un tipo de Component), mejor ser preciso y isar Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,5 +23,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(Integer idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(Integer idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 }
